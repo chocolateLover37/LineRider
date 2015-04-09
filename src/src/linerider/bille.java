1f9c5bca28207f75077f9eb74d00a1506e771695
ptitzebredu74 <em.richely@gmail.com>
2015-04-09 09:22:41 +0200

    J'ai ajouté des fichiers cool

---
+++ src/src/linerider/bille.java
@@ -0,0 +1,37 @@
+
+
+public class bille extends Objet {
+    double pente;
+    double ec;
+    double masse;
+    
+    public bille(double ax,double ay,double dx){
+       //* dy=0 au début, dx=cste (m*g) 
+         super(ax,ay,dx,0);
+         pente=0;
+         ec=0;
+         masse=10;
+         
+    }
+    
+    public  void acceleration() {
+        dy=dy-(pente*10);
+   
+    }
+    
+    public boolean energieCinetique(){
+      ec=(1/2)*masse*Math.pow(dx+dy,(1/2)); 
+      if(ec>100){
+            return false; 
+      }
+      else{
+          return true;
+      }
+    }
+    
+}
+        
+        
+        
+    
+}
