b7e6da0bfc25483c0803e92ce96b61acf8737858
Belettedu59 <noe.d.iab@hotmail.fr>
2015-05-07 09:37:19 +0200

    Ajout de GetCollisionBoxes

--- src/Bille.java
+++ src/Bille.java
@@ -9,7 +9,14 @@ public abstract class Bille extends Objet {
         super(ax,ay,vx,vy,aframe,NomImage);
         masse=1;
     }
-    
+    public Rectangle[] GetCollisionBoxes (){
+    	
+    	Rectangle[] pipi = new Rectangle[5];
+    	for (int i=0; i<5; i++){
+    		pipi[i] = new Rectangle((int) (x-(i+1)*h/5), (int) (y + rayon*(-i+5)/5),(int) (rayon*(i+1)*(2/5)),(int) (rayon*(5-i)*(2/5)));
+    	}
+    	return pipi;
+    }
     public void move(double pente) {
     	// cas contact  if (Collision==true)
         
