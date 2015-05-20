04e2498a46825b55b5d67f208079709895da0325
KriKi <kristina.kincelova@insa-lyon.fr>
2015-05-20 19:37:37 +0200

    bouton play et ca change de couleur

--- src/Jeu.java
+++ src/Jeu.java
@@ -14,7 +14,7 @@ public class Jeu extends JFrame{
 	public JPanel outils = new JPanel();
 	public JPanel couleurs = new JPanel();
 	public JButton jTrait, jCrayon, jPoubelle, jGomme;
-	public JButton jNoir, jRouge, jOrange, jVert, jBleu, jTest;
+	public JButton jNoir, jRouge, jOrange, jVert, jBleu, jTest, jPlay;
 	public JButton[] tabOutils;
 	public JButton[] tabCouleurs;
 	public GamePanel gamePanel = new GamePanel();
@@ -26,6 +26,8 @@ public class Jeu extends JFrame{
 	public BufferedImage ArrierePlan;
 	public Graphics buffer;
 	public Bille billeRouge;
+        public Rectangle frameBille;
+        public boolean play;
 	
 	public Jeu(){		
 		JPanel mainPanel=new JPanel();
@@ -35,11 +37,14 @@ public class Jeu extends JFrame{
 		
 		mainPanel.setLayout(new BorderLayout());
 		mainPanel.setBackground(Color.black);
+                frameBille= new Rectangle(100,100,40,40);
+                billeRouge= new Bille(frameBille);
 
-		jCrayon = new JButton(new ImageIcon(Jeu.class.getResource("Kcrayon.png")));
-		jTrait = new JButton(new ImageIcon(Jeu.class.getResource("Ktrait.png")));
+        jCrayon = new JButton(new ImageIcon(Jeu.class.getResource("Kcrayon.png")));
+        jTrait = new JButton(new ImageIcon(Jeu.class.getResource("Ktrait.png")));
         jPoubelle = new JButton(new ImageIcon(Jeu.class.getResource("Kpoubelle.png")));
         jGomme = new JButton(new ImageIcon(Jeu.class.getResource("Kgomme.png")));
+        jPlay=new JButton(new ImageIcon(Jeu.class.getResource("Kplay.png")));
         
         jNoir = new JButton(new ImageIcon(Jeu.class.getResource("Gnoir.png")));
         jRouge = new JButton(new ImageIcon(Jeu.class.getResource("Grouge.png")));
@@ -47,7 +52,7 @@ public class Jeu extends JFrame{
         jVert = new JButton(new ImageIcon(Jeu.class.getResource("Gvert.png")));
         jBleu = new JButton(new ImageIcon(Jeu.class.getResource("Gbleu.png")));
         
-        JButton[] tabOutils = {jTrait,jCrayon,jPoubelle,jGomme};
+        JButton[] tabOutils = {jTrait,jCrayon,jPoubelle,jGomme,jPlay};
         for(int i=0;i<tabOutils.length;i++){
                 tabOutils[i].setPreferredSize(new Dimension(50, 50));
                 tabOutils[i].addActionListener(new GestionToolBar());
@@ -100,14 +105,30 @@ public class Jeu extends JFrame{
 	
 	private class TimerAction implements ActionListener{
 		public void actionPerformed(ActionEvent e){
-			//boucle_principale_jeu();  c'etait dans space invaders
+			boucle_princip();  //c'etait dans space invaders
 			temps++;
 		}
 	}
+        
+        public  void boucle_princip(){
+            if(play){
+                billeRouge.move(temps);
+                repaint();
+            }
+        
+    }
     
-    public class GestionToolBar implements ActionListener{  
+    public class GestionToolBar implements ActionListener{ 
+        
+        
     	
         public void background(JButton b){
+            JButton[] tabOutils = {jTrait,jCrayon,jPoubelle,jGomme,jPlay};
+            for(int i=0;i<tabOutils.length;i++){
+                    tabOutils[i].setPreferredSize(new Dimension(50, 50));
+                    tabOutils[i].setBackground(Color.white);
+                    outils.add(tabOutils[i]);    
+            }
         	for(int i=0;i<tabOutils.length;i++){
         		if(tabOutils[i]==b){
         			tabOutils[i].setBackground(Color.blue);
@@ -167,6 +188,11 @@ public class Jeu extends JFrame{
 	        else if (e.getSource ()==jNoir){
 	        	gamePanel.setPointerColor(Color.black);
 	        }  
+                else if(e.getSource ()==jPlay){
+                        System.out.println("ca jouueeeee");
+                        play=true;
+                        repaint();
+            }
 	    	
     	} 
     }    
@@ -188,6 +214,13 @@ public class Jeu extends JFrame{
 		    }
         }
         return choc;
+    }
+    
+    public void paintComponent(Graphics g) {
+        billeRouge.draw(temps,buffer);
+        g.drawImage(ArrierePlan,0,0,this);
+       
+
     }
 }
 
