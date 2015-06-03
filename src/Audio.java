b7052b8bf93838a948cf2e50758b55ee8e1e847a
KriKi <kristina.kincelova@insa-lyon.fr>
2015-06-03 14:36:11 +0200

    what<

---
+++ src/Audio.java
@@ -0,0 +1,67 @@
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.SourceDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+public class Audio extends Thread {
+
+
+    AudioInputStream audioInputStream = null;
+    SourceDataLine line;
+
+    public void run() {
+        File fichier = new File("Jamiroquai");
+        try {
+            AudioFileFormat format = AudioSystem.getAudioFileFormat(fichier);
+        } catch (UnsupportedAudioFileException e1) {
+            e1.printStackTrace();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+
+        try {
+            audioInputStream = AudioSystem.getAudioInputStream(fichier);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        AudioFormat audioFormat = audioInputStream.getFormat();
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, audioFormat);
+
+        try {
+            line = (SourceDataLine) AudioSystem.getLine(info);
+
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            line.open(audioFormat);
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+            return;
+        }
+        line.start();
+        //Fenetre.begin=true;
+        try {
+            byte bytes[] = new byte[1024];
+            int bytesRead = 0;
+            while ((bytesRead = audioInputStream.read(bytes, 0, bytes.length)) != -1) {
+                line.write(bytes, 0, bytesRead);
+            }
+        } catch (IOException io) {
+            io.printStackTrace();
+            return;
+        }
+    }
+}
